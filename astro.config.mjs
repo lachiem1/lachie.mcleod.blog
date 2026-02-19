b4b02f678de1bab1d255038481cfdc412f17452f
lachiem1 <lachiem1@gmail.com>
2026-02-20 01:34:29 +1100

    Configure Astro for lachiem1.me custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
-const isProd = process.env.NODE_ENV === "production";
-
 export default defineConfig({
-  site: "https://lachiem1.github.io",
-  base: isProd ? "/lachie.mcleod.blog/" : "/"
+  site: "https://lachiem1.me",
+  base: "/"
 });
